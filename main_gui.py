69fd27425f7e963782d8c875ec7f5f16d0523ff3
Joon25 <openpostbox.lee@gmail.com>
2018-10-22 21:16:00 -0700

    Add colour.py

--- main_gui.py
+++ main_gui.py
@@ -1,13 +1,17 @@
 import  pygame, sys
 from    pygame.locals import *
+from    colour import *
 
 pygame.init()
 DISPLAY_GUI = pygame.display.set_mode((400,400))
 pygame.display.set_caption("2048")
 
+DISPLAY_GUI.fill(Black)
+
 while True:
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-  pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
