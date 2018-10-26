236124112dad73048e7e72273d2f4fdbda8ac215
Joon25 <40859693+Joon25@users.noreply.github.com>
2018-10-26 15:49:45 +0100

    Update colour.py

--- colour.py
+++ colour.py
@@ -1,16 +1,24 @@
-Aqua = (0,255,255)
-Black = (0,0,0)
-Blue = (0,0, 255)
-Fuchsia = (255, 0, 255)
-Gray = (128, 128, 128)
-Green = (0, 128, 0)
-Lime = (0, 255, 0)
-Maroon = (128, 0, 0)
-NavyBlue = (0,0, 128)
-Olive = (128, 128, 0)
-Purple = (128, 0, 128)
-Red = (255, 0, 0)
-Silver = (192, 192, 192)
-Teal = (0, 128, 128)
-White = (255, 255, 255)
-Yellow = (255, 255, 0)
+import pygame, sys, random
+from pygame.locals import *
+
+BORDER_COLOR = pygame.Color('BLUE')
+NUM_COLOR = pygame.Color('RED')
+DISABLE_COLOR = pygame.Color('aliceblue')
+1st_Color = pygame.Color('moccasin')        # 2
+2nd_Color = pygame.Color('navajowhite1')    # 4
+3rd_Color = pygame.Color('navajowhite2')    # 8
+4th_Color = pygame.Color('orange1')         # 16
+5th_Color = pygame.Color('olivedrab1')      # 32
+6th_Color = pygame.Color('lightpink1')      # 64
+7th_Color = pygame.Color('olivedrab1')      # 128
+8th_Color = pygame.Color('orchid1')         # 256
+9th_Color = pygame.Color('orange2')         # 512
+10th_Color = pygame.Color('olivedrab2')      # 1024
+11th_Color = pygame.Color('lightpink2')      # 2048
+12th_Color = pygame.Color('olivedrab2')      # 4096
+13th_Color = pygame.Color('orchid2')         # 8192
+14th_Color = pygame.Color('orange3')         # 16384
+15th_Color = pygame.Color('olivedrab3')      # 32768
+16th_Color = pygame.Color('lightpink3')      # 65536
+17th_Color = pygame.Color('olivedrab3')      # 131072
+18th_Color = pygame.Color('orchid3')         # 262144
